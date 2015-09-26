3ab810faf8be7b84f8875e2f42ae36bb1ae966d9
Raghav Sood <raghavsood@gmail.com>
2015-09-25 23:19:21 -0400

    Added creator activity

---
+++ Android/MyApplication2/app/src/main/java/com/hackgt2015project/ui/CreatorActivity.java
@@ -0,0 +1,19 @@
+package com.hackgt2015project.ui;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.hackgt2015project.R;
+
+public class CreatorActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_creator);
+	}
+	
+}
--- Android/MyApplication2/app/src/main/java/com/hackgt2015project/ui/MainActivity.java
+++ Android/MyApplication2/app/src/main/java/com/hackgt2015project/ui/MainActivity.java
@@ -1,9 +1,12 @@
 package com.hackgt2015project.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.hackgt2015project.R;
 
@@ -13,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button placeholder = (Button) findViewById(R.id.placeholder);
+		placeholder.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, CreatorActivity.class);
+				startActivity(intent);
+			}
+		});
     }
 
     @Override
