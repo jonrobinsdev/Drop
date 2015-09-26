d69047d3e753afa095ad14729e627a06148a68bb
Raghav Sood <raghavsood@gmail.com>
2015-09-26 02:13:01 -0400

    Fixing git fuck ups

--- Android/MyApplication2/app/src/main/java/com/hackgt2015project/ui/MainActivity.java
+++ Android/MyApplication2/app/src/main/java/com/hackgt2015project/ui/MainActivity.java
@@ -23,70 +23,46 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button signInButton;
+	private Button signInButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		FacebookSdk.sdkInitialize(getApplicationContext());
+		Parse.initialize(this, "WDxhZjVJSBXeBudlLDLU5RutPQnNU1gifhcD4Zuq", "8kg3SvqXKnnfo0PqabwLdv8Nv0iCOgEJ564qlknN");
+		ParseFacebookUtils.initialize(getApplicationContext());
 
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        Parse.initialize(this, "WDxhZjVJSBXeBudlLDLU5RutPQnNU1gifhcD4Zuq", "8kg3SvqXKnnfo0PqabwLdv8Nv0iCOgEJ564qlknN");
-        ParseFacebookUtils.initialize(getApplicationContext());
+		signInButton = (Button) findViewById(R.id.signInButton);
+		signInButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				logInWithFaceBook();
+			}
+		});
+	}
 
-        signInButton = (Button) findViewById(R.id.signInButton);
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                logInWithFaceBook();
-            }
-        });
-    }
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
+	}
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void logInWithFaceBook(){
-        final List<String> permissions = new ArrayList<String>();
-        permissions.add("user_friends");
-        //permissions.add("public_profile");
-        ParseFacebookUtils.logInWithReadPermissionsInBackground(this, permissions, new LogInCallback() {
-            @Override
-            public void done(ParseUser user, ParseException err) {
-                if (user == null) {
-                    Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
-                } else if (user.isNew()) {
-                    Log.d("MyApp", "User signed up and logged in through Facebook!");
-                } else {
-                    Log.d("MyApp", "User logged in through Facebook!");
-                }
-            }
-        });
-    }
+	public void logInWithFaceBook(){
+		final List<String> permissions = new ArrayList<String>();
+		permissions.add("user_friends");
+		//permissions.add("public_profile");
+		ParseFacebookUtils.logInWithReadPermissionsInBackground(this, permissions, new LogInCallback() {
+			@Override
+			public void done(ParseUser user, ParseException err) {
+				if (user == null) {
+					Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
+				} else if (user.isNew()) {
+					Log.d("MyApp", "User signed up and logged in through Facebook!");
+				} else {
+					Log.d("MyApp", "User logged in through Facebook!");
+				}
+			}
+		});
+	}
 }
